196bcc7cb43f5dfe0422375ff1c91b6a0c5e96da
Michael Crandall <44914172+MichaelCrandall@users.noreply.github.com>
2019-02-28 16:12:29 -0800

    Add files via upload

---
+++ main/cpp/Robot.cpp
@@ -0,0 +1,208 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+//320x240 might help with frames per second, reddit user said they got around 20-30 fps instead of our ~4 fps
+
+#include "Robot.h"
+
+#include <iostream>
+
+void Robot::RobotInit() {
+  compressor.Stop ();
+}
+
+void Robot::RobotPeriodic() {
+  frc::CameraServer::GetInstance()->StartAutomaticCapture(0);
+  frc::CameraServer::GetInstance()->StartAutomaticCapture(1);
+}
+
+void Robot::AutonomousInit() {
+//NO AUTONOMOUS
+}
+
+void Robot::AutonomousPeriodic() { //Same code for both Auto and Teleop, can't find a way to continue from one to other so both modes have the same code
+//Compressor
+  if (fightStick.GetRawButton(8) == 1) {
+    compressor.Start();
+  } else {
+    compressor.Stop();
+  }
+  
+//Drive
+  if (flightStick.GetRawButton(2) == 1) { //Slow
+    Drive.CurvatureDrive(flightStick.GetRawAxis(1) * -1 * speedSlow, flightStick.GetRawAxis(2) * turningSpeedFast, flightStick.GetRawButton(1));
+  } else if (flightStick.GetRawButton(12/*Will Change*/) == 1) { // Fast
+    Drive.CurvatureDrive(flightStick.GetRawAxis(1) * -1 * speedFast, flightStick.GetRawAxis(2) * turningSpeedFast, flightStick.GetRawButton(1));
+  } else { //Normal
+    Drive.CurvatureDrive(flightStick.GetRawAxis(1) * -1 * speedNormal, flightStick.GetRawAxis(2) * turningSpeedSlow, flightStick.GetRawButton(1));
+  }   
+//HAB Lift(???)
+  //Font
+  if (flightStick.GetRawButton(7) == 1) {
+    s_HABfront.Set(frc::DoubleSolenoid::kForward);
+  } else if (flightStick.GetRawButton(7) == 0) {
+    s_HABfront.Set(frc::DoubleSolenoid::kReverse);
+  } else {
+    s_HABfront.Set(frc::DoubleSolenoid::kOff);
+  }
+  //Back
+  if (flightStick.GetRawButton(8) == 1) {
+    s_HABback.Set(frc::DoubleSolenoid::kForward);
+  } else if (flightStick.GetRawButton(8) == 0) {
+    s_HABback.Set(frc::DoubleSolenoid::kReverse);
+  } else {
+    s_HABback.Set(frc::DoubleSolenoid::kOff);
+  }
+
+//Forklift Thomas special clutch idea for forklift
+    //POV is 0=UP 90=Right 180=Down 270=Left and 45 for all the angles
+    //.GetPOV("0") is the number for the POV system things switch
+  if (fightStick.GetPOV(0) == 315) { //Up
+    if (fightStick.GetRawButton(5) == 1) {
+      m_forklift.Set(upSpeedFast);
+    } else {
+      m_forklift.Set(upSpeedNormal);
+    }
+  } else if (fightStick.GetPOV(0) == 225) { //Down
+    if (fightStick.GetRawButton(5) == 1) {
+      m_forklift.Set(downSpeedFast);
+    } else {
+      m_forklift.Set(downSpeedNormal);
+    } 
+  } else if (fightStick.GetPOV(0) == 270) { //Stall
+    m_forklift.Set(stall);
+  } else {
+    m_forklift.Set(0);
+  }
+     
+//Ball Intake
+  if (fightStick.GetRawButton(1) == 1) {
+    m_leftIntake.Set(.5);//+
+    m_rightIntake.Set(-.5);
+  } else if (fightStick.GetRawButton(3) == 1) {
+    m_leftIntake.Set(-.5);
+    m_rightIntake.Set(.5);
+  } else {
+    m_leftIntake.Set(0);
+    m_rightIntake.Set(0);
+  }
+
+//Hatch Panel Launcher
+  //They are set backwards if time is avaible would like to switch them around in both the code and wiring
+  if (fightStick.GetRawButton(4) == 0) {
+    s_panelLauncherBottom.Set(frc::DoubleSolenoid::kForward);
+    s_panelLauncherTop.Set(frc::DoubleSolenoid::kForward);
+  } else if (fightStick.GetRawButton(4) == 1) {
+    s_panelLauncherBottom.Set(frc::DoubleSolenoid::kReverse);
+    s_panelLauncherTop.Set(frc::DoubleSolenoid::kReverse);
+  } else {
+    s_panelLauncherBottom.Set(frc::DoubleSolenoid::kForward);
+    s_panelLauncherTop.Set(frc::DoubleSolenoid::kForward);
+  }
+  //input.GetVoltage();
+}
+
+void Robot::TeleopInit() {
+//Nothing that I can think of
+}
+
+void Robot::TeleopPeriodic() {
+//Compressor
+  if (fightStick.GetRawButton(8) == 1) {
+    compressor.Start();
+  } else {
+    compressor.Stop();
+  }
+  
+//Drive
+  if (flightStick.GetRawButton(2) == 1) { //Slow
+    Drive.CurvatureDrive(flightStick.GetRawAxis(1) * -1 * speedSlow, flightStick.GetRawAxis(2) * turningSpeedFast, flightStick.GetRawButton(1));
+  } else if (flightStick.GetRawButton(12/*Will Change*/) == 1) { // Fast
+    Drive.CurvatureDrive(flightStick.GetRawAxis(1) * -1 * speedFast, flightStick.GetRawAxis(2) * turningSpeedFast, flightStick.GetRawButton(1));
+  } else { //Normal
+    Drive.CurvatureDrive(flightStick.GetRawAxis(1) * -1 * speedNormal, flightStick.GetRawAxis(2) * turningSpeedSlow, flightStick.GetRawButton(1));
+  }   
+//HAB Lift(???)
+  //Font
+  if (flightStick.GetRawButton(7) == 1) {
+    s_HABfront.Set(frc::DoubleSolenoid::kForward);
+  } else if (flightStick.GetRawButton(7) == 0) {
+    s_HABfront.Set(frc::DoubleSolenoid::kReverse);
+  } else {
+    s_HABfront.Set(frc::DoubleSolenoid::kOff);
+  }
+  //Back
+  if (flightStick.GetRawButton(8) == 1) {
+    s_HABback.Set(frc::DoubleSolenoid::kForward);
+  } else if (flightStick.GetRawButton(8) == 0) {
+    s_HABback.Set(frc::DoubleSolenoid::kReverse);
+  } else {
+    s_HABback.Set(frc::DoubleSolenoid::kOff);
+  }
+
+//Forklift Thomas special clutch idea for forklift
+    //POV is 0=UP 90=Right 180=Down 270=Left and 45 for all the angles
+    //.GetPOV("0") is the number for the POV system things switch
+  if (fightStick.GetPOV(0) == 315) { //Up
+    if (fightStick.GetRawButton(5) == 1) {
+      m_forklift.Set(upSpeedFast);
+    } else {
+      m_forklift.Set(upSpeedNormal);
+    }
+  } else if (fightStick.GetPOV(0) == 225) { //Down
+    if (fightStick.GetRawButton(5) == 1) {
+      m_forklift.Set(downSpeedFast);
+    } else {
+      m_forklift.Set(downSpeedNormal);
+    } 
+  } else if (fightStick.GetPOV(0) == 270) { //Stall
+    m_forklift.Set(stall);
+  } else {
+    m_forklift.Set(0);
+  }
+     
+//Ball Intake
+  if (fightStick.GetRawButton(1) == 1) {
+    m_leftIntake.Set(.5);//+
+    m_rightIntake.Set(-.5);
+  } else if (fightStick.GetRawButton(3) == 1) {
+    m_leftIntake.Set(-.5);
+    m_rightIntake.Set(.5);
+  } else {
+    m_leftIntake.Set(0);
+    m_rightIntake.Set(0);
+  }
+
+//Hatch Panel Launcher
+  //They are set backwards if time is avaible would like to switch them around in both the code and wiring
+  if (fightStick.GetRawButton(4) == 0) {
+    s_panelLauncherBottom.Set(frc::DoubleSolenoid::kForward);
+    s_panelLauncherTop.Set(frc::DoubleSolenoid::kForward);
+  } else if (fightStick.GetRawButton(4) == 1) {
+    s_panelLauncherBottom.Set(frc::DoubleSolenoid::kReverse);
+    s_panelLauncherTop.Set(frc::DoubleSolenoid::kReverse);
+  } else {
+    s_panelLauncherBottom.Set(frc::DoubleSolenoid::kForward);
+    s_panelLauncherTop.Set(frc::DoubleSolenoid::kForward);
+  }
+    //input.GetVoltage();
+}
+
+void Robot::TestPeriodic() {
+  double count = 0;
+  while (flightStick.GetRawAxis(1) > 0 || flightStick.GetRawAxis(1) < 0) {
+    Drive.CurvatureDrive((flightStick.GetRawAxis(1) / 5) * count, 1, 1);
+    frc::Wait(.01);
+    if (count < 5) {
+      count += .01;// For smoother grow
+    }
+  }
+}
+
+#ifndef RUNNING_FRC_TESTS
+int main() { return frc::StartRobot<Robot>(); }
+#endif
---
+++ main/include/Robot.h
@@ -0,0 +1,71 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <thread>
+#include <frc/AnalogInput.h>
+#include <frc/Compressor.h>
+#include <frc/DoubleSolenoid.h>
+#include <frc/Drive/DifferentialDrive.h>
+#include <frc/TimedRobot.h>
+#include <frc/Timer.h>
+#include <frc/Joystick.h>
+#include <frc/PowerDistributionPanel.h>
+#include <frc/PWMVictorSPX.h>
+#include <frc/Smartdashboard/SendableChooser.h>
+#include <frc/SpeedControllerGroup.h>
+#include <opencv2/core/core.hpp>//
+#include <opencv2/core/types.hpp>//
+#include <opencv2/imgproc/imgproc.hpp>//
+#include <wpi/raw_ostream.h>//
+#include <wpi/raw_ostream.h>//
+#include <cameraserver/CameraServer.h>//
+
+class Robot : public frc::TimedRobot {
+ public:
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void TestPeriodic() override;
+
+ private:
+  static constexpr double turningSpeedFast = .7;
+  static constexpr double turningSpeedSlow = .5;
+  static constexpr double speedFast = 1;
+  static constexpr double speedNormal = .7;
+  static constexpr double speedSlow = .5;
+  static constexpr double stall = -.2;
+  static constexpr double upSpeedNormal = -.4;
+  static constexpr double upSpeedFast = -.5;
+  static constexpr double downSpeedNormal = -.04;
+  static constexpr double downSpeedFast = -.03;
+  //Analog PI (PWM)
+  //frc::AnalogInput input{0};
+  frc::Joystick fightStick{1};
+  frc::Joystick flightStick{2};
+  frc::Compressor compressor;
+  frc::PWMVictorSPX m_frontLeft{0};
+  frc::PWMVictorSPX m_backLeft{1};
+  frc::PWMVictorSPX m_frontRight{2};
+  frc::PWMVictorSPX m_backRight{3};
+  frc::PWMVictorSPX m_leftIntake{4}; 
+  frc::PWMVictorSPX m_rightIntake{5};
+  frc::PWMVictorSPX m_forklift{6};
+  frc::SpeedControllerGroup m_left{m_frontLeft, m_backLeft};
+  frc::SpeedControllerGroup m_right{m_frontRight, m_backRight}; 
+  frc::DifferentialDrive Drive{m_left, m_right};
+  frc::DoubleSolenoid s_panelLauncherBottom{0, 1};
+  frc::DoubleSolenoid s_panelLauncherTop{2, 3};
+  frc::DoubleSolenoid s_HABfront{4, 5};
+  frc::DoubleSolenoid s_HABback{6, 7};
+};
