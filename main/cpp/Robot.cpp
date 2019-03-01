59996cb5ed9533883085cebb0d8e0cb02267581c
Michael Crandall <44914172+MichaelCrandall@users.noreply.github.com>
2019-02-28 19:23:50 -0800

    Update Robot.cpp

--- main/cpp/Robot.cpp
+++ main/cpp/Robot.cpp
@@ -16,8 +16,20 @@ void Robot::RobotInit() {
 }
 
 void Robot::RobotPeriodic() {
-  frc::CameraServer::GetInstance()->StartAutomaticCapture(0);
-  frc::CameraServer::GetInstance()->StartAutomaticCapture(1);
+  cs::UsbCamera floorCamera = frc::CameraServer::GetInstance()->StartAutomaticCapture(0);
+  cs::UsbCamera driverCamera = frc::CameraServer::GetInstance()->StartAutomaticCapture(1);
+  driverCamera.SetResolution(320, 240);
+  floorCamera.SetResolution(640, 480);
+  cs::CvSink cvSink = frc::CameraServer::GetInstance()->GetVideo();
+  cs::CvSource outputStream = frc::CameraServer::GetInstance()->PutVideo("Rectangle", 640, 480);
+  cv::Mat mat;
+  while (true) {
+    if (cvSink.GrabFrame(mat) == 0) {
+        outputStream.NotifyError(cvSink.GetError());
+      }
+      rectangle(mat, cv::Point(100, 100), cv::Point(400, 400), cv::Scalar(255, 255, 255), 5);
+      outputStream.PutFrame(mat);
+    }
 }
 
 void Robot::AutonomousInit() {
